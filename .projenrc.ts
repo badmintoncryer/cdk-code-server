c6b5c3bd59f6ca40d76b4ccd23d1147836f4d6e4
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-08-10 08:45:59 +0900

    init

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,16 +2,32 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Kazuho CryerShinozuka',
   authorAddress: 'malaysia.cryer@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.120.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
   name: 'cdk-code-server',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/badmintoncryer/cdk-code-server.git',
+  keywords: ['aws', 'cdk', 'ec2', 'nodejs', 'aws-cdk', 'vscode'],
+  gitignore: ['*.js', '*.d.ts', '!test/.*.snapshot/**/*', '.tmp'],
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  deps: [
+    '@open-constructs/aws-cdk',
+    'cdk-preinstalled-amazon-linux-ec2',
+  ],
+  description: 'CDK Construct for a VSCode Server development environment on EC2',
+  devDeps: [
+    '@aws-cdk/integ-runner@2.120.0-alpha.0',
+    '@aws-cdk/integ-tests-alpha@2.120.0-alpha.0',
+  ],
+  releaseToNpm: true,
+  packageName: 'cdk-code-server',
+  publishToPypi: {
+    distName: 'cdk-code-server',
+    module: 'cdk_code-server',
+  },
 });
+project.projectBuild.testTask.exec(
+  'yarn tsc -p tsconfig.dev.json && yarn integ-runner',
+);
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,93 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import * as ocf from '@open-constructs/aws-cdk';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { PreinstalledAmazonLinuxInstance, PreinstalledSoftwarePackage } from 'cdk-preinstalled-amazon-linux-ec2';
+import { Construct } from 'constructs';
+
+export interface CodeServerProps {
+  readonly vpc?: ec2.IVpc;
+  readonly instanceType?: ec2.InstanceType;
+  readonly cpuType?: ec2.AmazonLinuxCpuType;
+  readonly policy?: iam.PolicyStatement;
+  readonly volumeSize?: number;
+}
+
+export class CodeServer extends Construct {
+  constructor(scope: Construct, id: string, props: CodeServerProps = {}) {
+    super(scope, id);
+
+    const vpc = props.vpc ?? new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 1,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 26,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    const instance = new PreinstalledAmazonLinuxInstance(this, 'Instance', {
+      vpc,
+      instanceType: props.instanceType ?? ec2.InstanceType.of(
+        ec2.InstanceClass.C7G,
+        ec2.InstanceSize.XLARGE2,
+      ),
+      ssmSessionPermissions: true,
+      blockDevices: [
+        {
+        // Attach as a root device
+          deviceName: '/dev/xvda',
+          volume: ec2.BlockDeviceVolume.ebs(props.volumeSize ?? 30, {
+            deleteOnTermination: true,
+            encrypted: false,
+          }),
+        },
+      ],
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
+        cpuType: props.cpuType ?? ec2.AmazonLinuxCpuType.ARM_64,
+      }),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      preinstalledSoftware: {
+        packages: [
+          PreinstalledSoftwarePackage.NODEJS,
+          PreinstalledSoftwarePackage.VSCODE,
+          PreinstalledSoftwarePackage.GIT,
+        ],
+      },
+    });
+
+    instance.addUserData(
+      'npm install -g yarn',
+      // Install make and other build tools for setup of CDK
+      "sudo dnf groupinstall -y 'Development Tools'",
+      // Increase memory for Node.js
+      "echo 'export NODE_OPTIONS=--max-old-space-size=8192' >> /etc/profile.d/myenv.sh",
+      'chmod +x /etc/profile.d/myenv.sh',
+      // Enable linger for ec2-user to allow running services without a user logged in
+      'sudo loginctl enable-linger ec2-user',
+    );
+
+    instance.addToRolePolicy(
+      props.policy ?? new iam.PolicyStatement({
+        actions: ['*'],
+        resources: ['*'],
+      }),
+    );
+
+    const eicEndpoint = new ocf.aws_ec2.InstanceConnectEndpoint(
+      this,
+      'InstanceConnectEndpoint',
+      {
+        vpc,
+      },
+    );
+
+    // Opening Security Group from EIC Endpoint to EC2 Instance
+    eicEndpoint.connections.allowTo(instance, ec2.Port.tcp(22));
   }
 }
